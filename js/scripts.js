9acb6c4c30911da28b46546d43fb79299524ecab
Guest User <Guest@epicodus-20.local>
2018-01-25 08:38:53 -0800

    reset button functionally resets values after user clicks reset button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,56 @@
-var ticketValue = 8;
-
-function Ticket(movieType, movieTime, movieAge) {
+function Ticket(movieType, movieTime, movieAge, moviePrice) {
   this.movieType = movieType;
   this.movieTime = movieTime;
   this.movieAge = movieAge;
+  this.moviePrice = moviePrice
+
+
+  if (movieType === "new") {
+    this.moviePrice += 2;
+  }
+
+
+  if (movieTime === "matinee") {
+    this.moviePrice += 1;
+  } else if (movieTime === "prime") {
+    this.moviePrice += 2;
+  }
+
+
+  if (movieAge === "kids") {
+    this.moviePrice -= 2;
+  } else if (movieAge === "seniors") {
+    this.moviePrice -= 1;
+  };
+
 }
 
-// function findPrice (movieType, movieTime, movieAge) {
-//   if (Ticket.movieType === "new") {
-//     ticketValue += 2;
-//   }
-// }
-
-
-
-
-console.log(ticketValue);
-
 $(document).ready(function() {
+  var reset = function() {
+    var movieType = $("#movieType").val("new");
+    var movieTime = $("#movieTime").val("prime");
+    var movieAge = $("#movieAge").val("adults");
+    $("#show-price").text('')
+    $("#reset").hide();
 
+  }
   $("#movieForm").submit(function(event) {
     event.preventDefault();
     var movieType = $("#movieType").val();
     var movieTime = $("#movieTime").val();
     var movieAge = $("#movieAge").val();
+    var moviePrice = 8;
 
-    var ticketPrice = new Ticket(movieType, movieTime, movieAge);
-    console.log(ticketValue);
+    var ticket = new Ticket(movieType, movieTime, movieAge, moviePrice);
 
-    if (movieType === "new") {
-      ticketValue += 2;
-    };
-    if (movieTime === "matinee") {
-      ticketValue += 1;
-    } if (movieTime === "prime") {
-      ticketValue += 2;
-    } if (movieAge === "kids") {
-      ticketValue -= 2;
-    } if (movieAge === "seniors") {
-      ticketValue -= 1;
-    };
-    $("#show-price").text(ticketValue);
 
-    console.log(ticketValue);
+    var moviePriceDisplayText = ("Here is your movie ticket price ");
+
+    $("#show-price").text(moviePriceDisplayText + ticket.moviePrice);
+    $("#reset").show();
 
   });
+
+  $("#reset").click(reset)
+  reset();
 });
